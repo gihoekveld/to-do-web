478bced090a929a29ca934ec7dfa23e4e8296c26
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-10-19 21:27:17 -0300

    add to-do list and implements react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,88 @@ import styles from './App.module.css';
 
 import './global.css';
 import { ToDoList } from './components/ToDoList';
+import { useState } from 'react';
+
+const tasksList = [
+  {
+    id: 'bcb89cc4-6130-49d0-b39d-f1fd86db1779',
+    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    date: '2022-08-01T00:00:00.000Z',
+    isComplete: false
+  },
+  {
+    id: '764c7b98-c7c5-4362-a855-d79277bf71c2',
+    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    date: '2022-09-10T00:00:00.000Z',
+    isComplete: false
+  },
+  {
+    id: '28294ce0-0955-4550-806b-a00791abcdc2',
+    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    date: '2022-10-10T00:00:00.000Z',
+    isComplete: false
+  },
+  {
+    id: '10538f4a-722c-44a8-bbdf-bcd595e0c139',
+    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    date: '2022-10-11T00:00:00.000Z',
+    isComplete: true
+  },
+  {
+    id: '5718ab6f-8566-474e-980b-5d536bd534d5',
+    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+    date: '2022-10-13T00:00:00.000Z',
+    isComplete: true
+  }
+];
 
 function App() {
+  const [tasks, setTasks] = useState(tasksList);
+
+  function handleChangeTaskStatus(id: string) {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        }
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function handleDeleteTask(id: string) {
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
+  }
+
+  function handleCreateNewTask(title: string) {
+    const newTask = {
+      id: Math.random().toString(36).substring(2, 9),
+      title,
+      date: new Date().toISOString(),
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div>
-      <Header />
-
+      <Header onCreateNewTask={handleCreateNewTask} />
       <div className={styles.wrapper}>
-        <main>
-          <ToDoList />
+        <main className={styles.main}>
+          <ToDoList 
+            tasks={tasks}
+            onChangeTaskStatus={handleChangeTaskStatus}
+            onDeleteTask={handleDeleteTask}
+          />
         </main>
       </div>
-      
     </div>
   )
 }
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -2,42 +2,31 @@ import { EmptyState } from './EmptyState';
 import { Task } from './Task';
 import styles from './ToDoList.module.css';
 
-const tasks = [
-  {
-    id: 1,
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete: false
-  },
-  {
-    id: 2,
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete: false
-  },
-  {
-    id: 3,
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete: false
-  },
-  {
-    id: 4,
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete: true
-  },
-  {
-    id: 5,
-    title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete: true
-  }
-];
+interface ToDoListProps {
+  tasks: {
+    id: string;
+    title: string;
+    date: string;
+    isComplete: boolean;
+  }[];
+  onChangeTaskStatus: (id: string) => void;
+  onDeleteTask: (id: string) => void;
+}
 
-export function ToDoList () {
+export function ToDoList ({tasks, onChangeTaskStatus, onDeleteTask}: ToDoListProps) {
   const countTasks = tasks.length;
 
   const isEmtpyList = countTasks === 0;
 
-  const completesTasks = tasks.filter(task => task.isComplete);
+  const countCompleteTasks = tasks.filter(task => task.isComplete).length;
 
-  const countCompleteTasks = completesTasks.length;
+  function handleChangeTaskStatus(id: string) {
+    onChangeTaskStatus(id);
+  }
+
+  function handleDeleteTask(id: string) {
+    onDeleteTask(id);
+  }
 
   return (
     <div>
@@ -52,7 +41,23 @@ export function ToDoList () {
         </div>
       </header>
       <main className={styles.main}>
-        {isEmtpyList ? <EmptyState /> : <Task />}
+        {
+          isEmtpyList
+          ? <EmptyState /> 
+          : (tasks.map(task => {
+              return (
+                <Task 
+                  key={task.id} 
+                  id={task.id}
+                  title={task.title} 
+                  isComplete={task.isComplete}
+                  onChangeTaskStatus={() => handleChangeTaskStatus(task.id)}
+                  onDeleteTask={() => handleDeleteTask(task.id)}
+                />
+              )
+            })
+          )
+        }
       </main>
     </div>
   )
