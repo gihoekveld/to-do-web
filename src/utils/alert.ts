99b8cab4c6792cd265fdd8fd034516263feae906
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-10-29 20:01:08 -0300

    create alert functions with react-toastify

---
+++ src/utils/alert.ts
@@ -0,0 +1,28 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export function successAlert(message: string) {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+}
+
+export function errorAlert(message: string) {
+  toast.error(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+}
